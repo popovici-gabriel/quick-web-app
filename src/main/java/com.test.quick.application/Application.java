f0a88405d81234410a4688d16258fe3eb043703b
Gabriel Popovici <popovici.gabriel@gmail.com>
2018-10-30 01:19:09 +0200

    Update sample quick message

---
+++ src/main/java/com.test.quick.application/Application.java
@@ -0,0 +1,22 @@
+package com.test.quick.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@EnableAutoConfiguration
+@RestController
+public class Application {
+
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
