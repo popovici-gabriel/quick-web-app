ca5a0ab694b49f658839ba8e3137dedba16c95c1
Gabe <popovici.gabriel@gmail.com>
2018-10-30 21:08:42 +0200

    Update sample quick message

---
+++ src/test/java/com/test/quick/application/home/GreetingResourceIntegrationTest.java
@@ -0,0 +1,36 @@
+package com.test.quick.application.home;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.client.Traverson;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.net.URI;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class GreetingResourceIntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    /*
+     * {"content":"Hello, World!","_links":{"self":{"href":"http://localhost:8080/greeting?name=World"}}}
+     *
+     * @throws Exception
+     */
+    @Test
+    public void envEndpointNotHidden() throws Exception {
+        Traverson traverson = new Traverson(new URI("http://localhost:" + this.port + "/greeting"), MediaTypes.HAL_JSON);
+        String greeting = traverson.follow("self").toObject("$.content");
+        assertThat(greeting).isEqualTo("Hello, World!");
+    }
+
+
+}
